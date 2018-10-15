e73d611fab7b0137e951b0d41bdad76f9c117d93
numoonchld <raghavendra.saralaya@gmail.com>
2018-10-15 20:57:03 +0530

    (#4) displaying a list of heros

---
+++ src/app/heroes/mock-heroes.ts
@@ -0,0 +1,15 @@
+
+
+
+export const HEROES = [
+    {id: 11, name: 'Shaktimaan'},
+    {id: 12, name: 'Karma'},
+    {id: 13, name: 'Captain Vyom'},
+    {id: 14, name: 'Venkateshwara'},
+    {id: 15, name: 'Krishna'},
+    {id: 16, name: 'Shiva'},
+    {id: 17, name: 'Mitra'},
+    {id: 18, name: 'Lakshmi'},
+    {id: 19, name: 'Vindhya'},
+    {id: 20, name: 'Shani'},
+];
