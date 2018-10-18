73df5c97ddf7cad4a54cd98b35d3c4409b783072
numoonchld <raghavendra.saralaya@gmail.com>
2018-10-19 01:01:29 +0530

    (6) part b: service-within-service show message only on hero selection

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Hero } from './heroes/hero';
 import { HEROES } from './heroes/mock-heroes';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 
 @Injectable({   // Flags class in this component to be a participant of Dependency Injection (DI) system
@@ -11,8 +12,11 @@ import { Observable, of } from 'rxjs';
 export class HeroService {  // This class is the Provider of service called "HeroService"
 
   getHeroes(): Observable<Hero[]> {
+    // TODO: send the message _after_ fetching the heroes
+    this.messageservice.add("HeroService: fetched heroes");
     return of(HEROES);
-  }
 
-  constructor() { }
+  }
+  
+  constructor(private messageservice: MessageService) { }
 }
