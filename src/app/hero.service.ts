1e14174b81a89dff5be5dd93efb2a696d3fd50f8
numoonchld <raghavendra.saralaya@gmail.com>
2018-10-31 21:03:01 +0530

    (8) Http get for hero item works

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,6 +3,7 @@ import { Hero } from './heroes/hero';
 import { HEROES } from './heroes/mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({   // Flags class in this component to be a participant of Dependency Injection (DI) system
   providedIn: 'root' // Decides scope of service availabity, root provides serives for components throughout the app
@@ -10,18 +11,23 @@ import { MessageService } from './message.service';
 
 export class HeroService {  // This class is the Provider of service called "HeroService"
 
+  private heroesUrl = 'api/heroes'; // URL to web api
+
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
-    this.messageService.add("HeroService: fetched heroes");
-    return of(HEROES);
-
+    this.messageService.add('HeroService: fetched heroes');
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
-  
+
   getHero(id: number): Observable<Hero> {
     // TODO: send the message _after_ fetching the hero
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
   }
 
-  constructor(private messageService: MessageService) { }
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  constructor(private http: HttpClient, private messageService: MessageService) { }
 }
