432b2f231317f3cdc0adcd484c3ccd63723f1cb5
numoonchld <raghavendra.saralaya@gmail.com>
2018-11-01 01:50:51 +0530

    (8) deleting heros feature functional

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -19,6 +19,25 @@ export class HeroService {  // This class is the Provider of service called "Her
 
   private heroesUrl = 'api/heroes'; // URL to web api
 
+  /** POST: add a new hero to the server */
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
+      tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
+  /** DELETE: delete the hero from the server */
+  deleteHero(hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+
   /**
  * Handle Http operation that failed.
  * Let the app continue.
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,6 +8,8 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 
+
+
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
@@ -16,6 +18,20 @@ export class HeroesComponent implements OnInit {
   // onSelect(hero: Hero): void {
   //   this.selectedHero = hero;
   // }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
